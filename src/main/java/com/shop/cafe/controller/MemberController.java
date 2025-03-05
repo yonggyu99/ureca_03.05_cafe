e50bc8ffd2a958af56e6ab8951968e01bc2e7d90
Yonggyu <yoyo5411@naver.com>
2025-03-05 15:17:19 +0900

    insertMember 완료

--- src/main/java/com/shop/cafe/controller/MemberController.java
+++ src/main/java/com/shop/cafe/controller/MemberController.java
@@ -1,14 +1,40 @@
 package com.shop.cafe.controller;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class MemberController {
+import com.shop.cafe.dto.Member;
+import com.shop.cafe.service.MemberService;
 
-	@GetMapping("insertMember")
+@RestController
+@CrossOrigin("http://127.0.0.1:5500/")
+public class MemberController {
 	
-	public String insertMember() {
-		return "ok";
+	@Autowired
+	MemberService memberService;
+	
+	@PostMapping("insertMember")
+	
+	public Map<String, String> insertMember(@RequestBody Member m) {
+		
+		Map<String, String> responseData = new HashMap();
+		try {
+			memberService.insertMember(m);
+			responseData.put("msg", "ok");
+			
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+			responseData.put("msg", e.getMessage());
+		}
+		
+		return responseData;
 	}
 }
